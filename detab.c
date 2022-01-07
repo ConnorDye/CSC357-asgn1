48a0f053cae3646d117e483d0b49aeb97bf4edda
ConnorDye <connor.h.dye@gmail.com>
2022-01-07 14:19:48 -0800

    detab.c

---
+++ detab.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <iostream>
+
+#define TAB 8
+
+int main(int argc, char *argv[]){
+	int currPos = 0;
+	int c; /* c will represent character, however chars are integer values*/
+	int blanks; /* # of blanks to add */
+
+	while((c=getchar() != EOF){
+		if(c != '\t' && c != '\n'){
+			putchar(c); /*If c isn't a tab or newLine */
+			currPos++;
+		}
+		else if(c == '\t'){
+			blanks = TAB - (currPos % TAB);
+		       	while(blanks != 0){
+				putchar(' ');
+				blanks--;
+				currPos++;
+			}
+		}
+		else if(c == '\n'){
+			currPos = 0;
+			putchar(c);
+	
+		}
+	}
+
+	return 0;
+}
